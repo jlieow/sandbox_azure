49ad6e33a42fd4db0fa9d007bf3c2c1803ceb2ec
jlieow <52845474+jlieow@users.noreply.github.com>
2023-02-15 10:35:05 +0800

    Update names for terraform blocks to become more relevant

--- oauth2.0_with_app_registrations/modules/oauth2/main.tf
+++ oauth2.0_with_app_registrations/modules/oauth2/main.tf
@@ -45,14 +45,24 @@ resource "azuread_application_pre_authorized" "client" {
   permission_ids = [ random_uuid.test_oauth2_permission_scope_id.result ]
 }
 
-data "azurerm_api_management" "example" {
+data "azurerm_api_management" "get_apim" {
   name = "jerome-test-apim"
   resource_group_name = "jerome-test-env"
 }
 
-resource "azurerm_api_management_authorization_server" "example" {
-  api_management_name = data.azurerm_api_management.example.name
-  resource_group_name = data.azurerm_api_management.example.resource_group_name
+# resource "azurerm_api_management" "apim" {
+#   name                = "example-apim"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   publisher_name      = "My Company"
+#   publisher_email     = "company@terraform.io"
+
+#   sku_name = "Consumption"
+# }
+
+resource "azurerm_api_management_authorization_server" "authorization_server" {
+  api_management_name = data.azurerm_api_management.get_apim.name
+  resource_group_name = data.azurerm_api_management.get_apim.resource_group_name
 
   name = "test"
   display_name = "test"
