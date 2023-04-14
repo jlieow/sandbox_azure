fb00f1a994de12b8eadac78df7e5b092111c4458
jlieow <52845474+jlieow@users.noreply.github.com>
2023-04-14 13:15:15 +0800

    Example Azure

---
+++ azure_vm/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+  # required_version = ">=1.2.7, <=1.3.6"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+# Get local IP address
+data "http" "icanhazip" {
+   url = "https://ipv4.icanhazip.com/"
+}
+
---
+++ azure_vm/output.tf
@@ -0,0 +1,3 @@
+output "azuread_application" {
+  value = module.oauth2.azuread_application
+}
